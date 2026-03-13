6643b5dc1df4a0659a63caab3633a80b2cb42c3b
aloreedelaforet37 <a.l.oree.de.la.foret.37@gmail.com>
2026-03-13 23:07:35 +0100

    Update maintenance.js

--- maintenance.js
+++ maintenance.js
@@ -2,6 +2,6 @@ const MAINTENANCE = true;
 
 if (MAINTENANCE) {
   if (!window.location.pathname.includes("maintenance.html")) {
-    window.location.replace("/reservation-test/maintenance.html");
+    window.location.replace("/demande-reservation/maintenance.html");
   }
 }
