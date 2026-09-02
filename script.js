21de97b6bb7e000c92ce4c9bac233e8fb80dcf4f
aloreedelaforet37 <a.l.oree.de.la.foret.37@gmail.com>
2026-09-02 09:33:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -93,8 +93,7 @@ function hideWaiting() {
   ];
 
   const datesCompletes = [
-    { debut: "2026-07-26", fin: "2026-09-03" },
-    { debut: "2026-09-08", fin: "2026-09-20" }
+    { debut: "2026-07-26", fin: "2026-09-20" }
   ];
 
   const encartFermeture = document.getElementById("encartFermeture");
