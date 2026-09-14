12767eb6ab37f63744986eaee342c8f26b89aed7
aloreedelaforet37 <a.l.oree.de.la.foret.37@gmail.com>
2026-09-14 23:38:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ function hideWaiting() {
 
   const datesCompletes = [
     { debut: "2026-09-10", fin: "2026-09-21" },
-    { debut: "2026-09-25", fin: "2026-09-30" }
+    { debut: "2026-09-25", fin: "2026-10-06" }
   ];
   
   const datesIndisponibles = [
